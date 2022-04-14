27d284a9cb1fcd029aa3f4bd68ffadfd6684d8fc
罗杰 <13369189710@163.com>
2022-04-14 15:59:00 +0800

    确认 json 字段类型：当整个 json 中没有负数时，所有的数字类型都是 unsigned，若有一个负数，则所有的数字类型变成有符号的

--- json/main.cpp
+++ json/main.cpp
@@ -18,24 +18,39 @@
 #include <stdlib.h>
 #include <json.hpp>
 #include <iostream>
+#include <cstdio>
 
 using namespace std;
 using json = nlohmann::json;
 
 int main() {
-    auto j3 = json::parse("{ \"happy\": true, \"pi\": 3.141 }");
+    auto j3 = json::parse("{ \"happy\": true, \"pi\": 3.141, \"id\": 1 }");
     cout << j3 << endl;
     cout << "happy: " << j3["happy"] << endl;
     cout << "size: " << j3.size() << endl;
+    auto t = j3["id"].type();
+    printf("id type: %d\n", t);
+    // cout << "happy type: " << t << endl;
 
     json j;
     j["name"] = "罗杰";
     j["age"] = 33;
+    j["money"] = -10;
     j["number"] = 100;
+    j["number2"] = 500000015399052129;
     j["phone"] = "15399052129";
     j["children"] = {"罗明灿", "罗灿宇"};
     j["data"] = {{"C++","Good"},{"Go","Greate"}};
     cout << "j.dump(): " << j.dump() << endl;
     cout << "j.dump(4): " << endl << j.dump(4) << endl;
+    // cout << "j[\"money\"].type(): " << j["money"].type() << endl;
+    // cout << j["money"].type() << endl;
+    
+    printf("age type: %d money type: %d number type: %d number2 type: %d\n", 
+            j["age"].type(), j["money"].type(), j["number"].type(), j["number2"].type());
+    /*
+    auto result = json::parse(j.dump());
+    cout << result["money"].type() << endl;
+    */
     return 0;
 }
