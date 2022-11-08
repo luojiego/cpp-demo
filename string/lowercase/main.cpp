46fe8ea0a434db588f8db5a7e5fa8e117f8675a6
罗杰 <13369189710@163.com>
2022-11-08 18:07:25 +0800

    add test

--- string/lowercase/main.cpp
+++ string/lowercase/main.cpp
@@ -4,6 +4,14 @@
 
 using namespace std;
 
+std::string tolowerAllianceName(const std::string& name) {
+    auto new_name = name;
+    std::transform(new_name.begin(), new_name.end(), new_name.begin(), [](unsigned char c){
+        return std::tolower(c);
+    });
+    return new_name;
+}
+
 int main() {
     std::string name = "Hello World!";
     std::transform(name.begin(), name.end(), name.begin(), [](unsigned char c){
@@ -11,9 +19,10 @@ int main() {
     });
     std::cout << name << std::endl;
 
-    // std::string name1 = "Hello World!";
-    // std::for_each(name1.begin(), name1.end(), std::tolower);
-    // std::cout << name1 << std::endl;
+    std::cout << tolowerAllianceName("ABC") << std::endl;
+    std::cout << tolowerAllianceName("我是中国人A") << std::endl;
+    std::cout << tolowerAllianceName("A我b是c中D国E人!") << std::endl;
+    std::cout << tolowerAllianceName("不要熬夜") << std::endl;
     return 0;
 }
 
